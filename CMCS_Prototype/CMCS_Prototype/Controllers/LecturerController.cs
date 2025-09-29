3485e7f1024f94f9883d4517fa409a5b155d466c
ST10329226 <128092599+ST10329226@users.noreply.github.com>
2025-09-29 20:54:47 +0200

    feat: initial ASP.NET Core MVC project setup

---
+++ CMCS_Prototype/CMCS_Prototype/Controllers/LecturerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// This controller handles the pages specific to the Lecturer role
+public class LecturerController : Controller
+{
+    // the main Lecturer Dashboard view
+    public IActionResult Dashboard()
+    {
+        //tells the MVC framework to look in Views/Lecturer/Dashboard.cshtml
+        return View();
+    }
+    //Claim Submission Form view
+    public IActionResult SubmitClaim()
+    {
+        // This tells the MVC framework to look in Views/Lecturer/SubmitClaim.cshtml
+        return View();
+    }
+}
